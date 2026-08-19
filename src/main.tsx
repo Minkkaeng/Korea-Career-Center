bc004597eb1e07e627b05422b7f6cb4a488f6a9d
minkyoung.choi <choi90326@gmail.com>
2026-08-19 22:02:43 +0900

    main.tsx 업데이트

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Korea-Career-Center">
       <App />
     </BrowserRouter>
   </StrictMode>,
